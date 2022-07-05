1be9aa4f501ec04a1b36f195ba069238542a4ae3
rpalloni <rpdev20@gmail.com>
2022-07-05 18:23:06 +0200

    external

--- dwh/snowflake/query/ddl.sql
+++ dwh/snowflake/query/ddl.sql
@@ -16,9 +16,13 @@ SELECT * FROM users;
 
 INSERT INTO USERS values('003', 'cuser', 26);
 
-
-PUT file:///tmp/snowflaketmp/usersdata.csv @%users; -- stage the file in an internal location
+-- via SnowSQL https://community.snowflake.com/s/question/0D50Z00007r3NloSAE/the-command-is-not-supported-from-the-ui-put
+CREATE OR REPLACE STAGE users_stage;
+PUT file:///tmp/snowdata/usersdata.csv @users_stage; -- stage the file in an internal LOCATION
+LIST @users_stage; -- verify the list of files staged successfully
 COPY INTO users; -- copy into the table
+
+
 
 
 
@@ -52,5 +56,13 @@ CREATE TABLE customer_shlw_copy LIKE customers; -- only structure
 -- temporary and transient
 CREATE TEMPORARY TABLE customer_temp AS SELECT * FROM customers WHERE try_to_number(postal_code) IS NOT NULL;
 CREATE TRANSIENT TABLE customer_trans AS SELECT * FROM customers WHERE try_to_number(postal_code) IS NULL;
+
+-- external
+CREATE OR REPLACE STAGE s3store URL='s3://snowflake-cookbook/Chapter02/r4/';
+LIST @s3store;
+
+CREATE OR REPLACE EXTERNAL TABLE tbl_ext WITH LOCATION = @s3store file_format = (TYPE=parquet);
+SELECT * FROM tbl_ext; -- external table always have JSON data
+
 
 SHOW TABLES LIKE 'customer%'
